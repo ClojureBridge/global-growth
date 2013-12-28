53e3817c35616f28c6464e7864435249f4eb281c
Bridget Hillyer <bridget.hillyer@gmail.com>
2013-12-28 11:57:57 -0500

    Passing query string as param map

--- src/cheap_life/core.clj
+++ src/cheap_life/core.clj
@@ -1,65 +1,64 @@
 (ns cheap-life.core
   (:require [clj-http.client :as client])
-  (:require [cheshire.core :as json])
-  (:gen-class))
+  (:require [cheshire.core :as json]))
 
 ;; CONSTANTS
 (def BASE_URI "http://api.worldbank.org")
 (def GAS_INDICATOR "EP.PMP.SGAS.CD")
 (def GDP_INDICATOR "NY.GDP.PCAP.CD")
 
-(defn- parse-json [str]
+(defn parse-json [str]
   (json/parse-string str true))
 
-(defn get-api [query-str]
+(defn get-api [path qp]
   "Returns json object representing API response"
-  (parse-json (:body 
-    (client/get 
-      (str BASE_URI
-          query-str 
-          "&format=json" )))))
+  (let 
+    [base_path (str BASE_URI path)
+     query-params (merge qp {:format "json"})]
+    (parse-json (:body 
+                  (client/get base_path {:query-params query-params})))))
 
-(defn get-value [query-str key]
+(defn get-value [path query-params key]
   "Returns single value from API response"
-  (get-in (last (get-api query-str)) [0 key]))
+  (get-in (last (get-api path query-params)) [0 key]))
 
-(defn get-value-map [query-str]
+(defn get-value-map [path query-params]
   "Returns map of values from API response"
   ;TODO: handle paging
-  (last (get-api query-str)))
-  
+  (last (get-api path query-params)))
 
 (defn get-indicator-all [indicator year key]
   "Returns indicator for a specified year for all countries"
   (get-value-map (str "/countries"
-                   "/all"
-                   "/indicators"
-                   "/" indicator
-                   "?date=" year)
-                ))
+                      "/all"
+                      "/indicators"
+                      "/" indicator)
+                 {:date year}))
 
 (defn get-indicator [indicator country year key]
   "Returns indicator for a country for a specified year"
   (get-value (str "/countries"
-                "/" country
-	        "/indicators"
-	        "/" indicator
-	        "?date=" year)
-           key ))
+                  "/" country
+                  "/indicators"
+                  "/" indicator)
+             {:date year}
+             key ))
 
 (defn get-gas [country year]
   "Returns the pump price for gasoline (US$ per liter) 
 	in a country for a specified year"
   (get-indicator GAS_INDICATOR country year :value))
 
-(defn get-gdp [country year]
+(defn get-gdp 
   "Returns the GDP per capita (current US$)
         in a country for a specified year"
+  [country year]
   (get-indicator GDP_INDICATOR country year :value))
           
-(defn get-gdp-all [year]
+(defn get-gdp-all 
   "Returns the GDP per capita (current US$)
         for a specified year for all countries"
+  [year]
   (get-indicator-all GDP_INDICATOR year :value))
 
 
