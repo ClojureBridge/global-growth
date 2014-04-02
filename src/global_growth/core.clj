77d3f6e74654dbeacbaee7cd14b3c91a14de72eb
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-04-01 23:09:52 -0400

    Removing and simplifying code
    I'm removing all code that we can, and trying to simplify the rest.
    The World Bank API code has all been pulled into its own namespace.

--- src/global_growth/core.clj
+++ src/global_growth/core.clj
@@ -1,88 +1,16 @@
 (ns global-growth.core
-  (:require [compojure.core :refer :all]
-            [ring.middleware.params :refer :all]
-            [clojure.set :as set]
-            [clj-http.client :as client]
-            [cheshire.core :as json]
-            [ring.adapter.jetty :as jetty]
+  (:require [global-growth.world-bank :as world-bank]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.handler :refer [site]]
             [hiccup.core :as hiccup]
             [hiccup.page :as page]
             [hiccup.form :as form]))
 
-;TODO: handle paging: right now just getting a large value
+; TODO: handle paging: right now just getting a large value
 ; for 'per_page' parameter.
 ; Maybe that is fine for simplicity. But that could be a
 ; problem if we get things with large result sets. Not
 ; getting large results as of now.
-
-;; CONSTANTS
-(def base-uri "http://api.worldbank.org")
-(def list-size 10)
-
-(defn parse-json [str]
-  (json/parse-string str true))
-
-;; WORLD BANK API CALLS
-(defn get-api
-  "Returns map representing API response."
-  [path qp]
-  (let [base-path (str base-uri path)
-        query-params (merge qp {:format "json" :per_page 10000})
-        response (parse-json (:body (client/get base-path {:query-params query-params})))
-        metadata (first response)
-        results (second response)]
-    {:metadata metadata
-     :results results}))
-
-(defn get-value-map
-  "Returns relation of two keys from API response"
-  [path query-params key1 key2]
-  (let [response (get-api path query-params)]
-    (into {} (for [item (:results response)]
-               [(key1 item) (key2 item)]))))
-
-(defn get-indicator-map []
-  "Gets map of indicators.
-  /topics/16/indicators:   All urban development
-  --- Other possibilities ---
-  /sources/2/indicators:   All world development indicators (about 1300)
-  /indicators:             All Indicators (about 8800)"
-  (get-value-map "/topics/16/indicators" {} :name :id))
-
-(defn remove-aggregate-countries
-  "Remove all countries that aren't actually countries, but are aggregates."
-  [countries]
-  (remove (fn [country]
-            (= (get-in country [:region :value]) "Aggregates")) countries))
-
-(defn get-country-ids
-  "Get set of country ids so we can filter out aggregate values."
-  []
-  (let [countries (remove-aggregate-countries (:results (get-api "/countries" {})))]
-    (set (map :iso2Code countries))))
-
-(def indicator-map (get-indicator-map))
-(def country-ids (get-country-ids))
-
-(defn get-indicator-all
-  "Returns indicator for a specified year for all countries"
-  [indicator year key1 key2]
-  (get-value-map (str "/countries"
-                      "/all"
-                      "/indicators"
-                      "/" indicator)
-                 {:date (str year)}
-                  key1
-                  key2))
-
-(defn sorted-indicator-map
-  "Sort the map of indicator numeric values"
-  [inds]
-  (take list-size
-        (sort-by val >
-                 (into {} (for [[k v] inds
-                                :when (and v (country-ids (:id k)))]
-                            [(:value k) (read-string v)])))))
 
 ;; WEB APP
 
@@ -124,21 +52,23 @@
 
 (defn view-indicators
   [indicator1 indicator2 year]
-  (let [inds1 (sorted-indicator-map
-                (get-indicator-all indicator1 year :country :value))
-        inds2 (sorted-indicator-map
-                (get-indicator-all indicator2 year :country :value))]
+  (let [inds1 (world-bank/get-indicator-values indicator1 year)
+        inds2 (world-bank/get-indicator-values indicator2 year)
+        indicator-map (into {}
+                            (map (fn [indicator]
+                                   [(second indicator) (first indicator)])
+                                 (world-bank/get-indicators)))]
   (layout "Sorted Indicators"
           [:h1 "Sorted Indicators"]
           [:div.row
            [:div.form-group.col-md-6
-            (form/label indicator1 (get (set/map-invert indicator-map) indicator1))
+            (form/label indicator1 (get indicator-map indicator1))
             (if (empty? inds1)
               [:p "No indicator values for this year."]
               (indicator-list inds1))
             ]
            [:div.form-group.col-md-6
-            (form/label indicator2 (get (set/map-invert indicator-map) indicator2))
+            (form/label indicator2 (get indicator-map indicator2))
             (if (empty? inds2)
               [:p "No indicator values for this year."]
               (indicator-list inds2))]])))
@@ -151,13 +81,20 @@
                      [:div.row
                       [:div.form-group.col-md-5
                        (form/label "indicator1" "Indicator 1:  ")
-                       (form/drop-down {:class "form-control"} "indicator1" (seq indicator-map))]
+                       (form/drop-down {:class "form-control"}
+                                       "indicator1"
+                                       (world-bank/get-indicators))]
                       [:div.form-group.col-md-5
                        (form/label "indicator2" "Indicator 2:  ")
-                       (form/drop-down {:class "form-control"} "indicator2" (seq indicator-map))]
+                       (form/drop-down {:class "form-control"}
+                                       "indicator2"
+                                       (world-bank/get-indicators))]
                       [:div.form-group.col-md-2
                        (form/label "year" "Year: ")
-                       (form/drop-down {:class "form-control"} "year" (reverse (range 1960 2013)) 2010 )]]
+                       (form/drop-down {:class "form-control"}
+                                       "year"
+                                       (reverse (range 1960 2013))
+                                       2010)]]
                      (form/submit-button "Submit"))))
 
 (defroutes main-routes
@@ -165,5 +102,5 @@
   (GET "/indicators" [indicator1 indicator2 year]
         (view-indicators indicator1 indicator2 year)))
 
-(def handler (wrap-params main-routes))
+(def handler (site main-routes))
 
